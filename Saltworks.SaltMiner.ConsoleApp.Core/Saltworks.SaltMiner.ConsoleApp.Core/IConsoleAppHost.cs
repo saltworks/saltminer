8a85c69cfa662a9248a96c5c5b8af85ee99a6e8d
bryant-sw <bryant@saltworks.io>
2025-03-26 10:31:54 -0500

    Initial Saltworks.SaltMiner.ConsoleApp.Core commit of ADO master branch

---
+++ Saltworks.SaltMiner.ConsoleApp.Core/Saltworks.SaltMiner.ConsoleApp.Core/IConsoleAppHost.cs
@@ -0,0 +1,13 @@
+﻿namespace Saltworks.SaltMiner.ConsoleApp.Core
+{
+    public interface IConsoleAppHost
+    {
+        void Run(IConsoleAppHostArgs args);
+    }
+
+    public interface IConsoleAppHostArgs
+    {
+        CancellationToken CancelToken { get; set; }
+        string[] Args { get; set; }
+    }
+}
---
+++ Saltworks.SaltMiner.ConsoleApp.Core/Saltworks.SaltMiner.ConsoleApp.Core/IConsoleAppHostBuilder.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Saltworks.SaltMiner.ConsoleApp.Core
+{
+    public interface IConsoleAppHostBuilder
+    {
+        public IConsoleAppHostBuilder BuildConfiguration();
+        public IConsoleAppHostBuilder ConfigureServices(Action<IServiceCollection, IConfiguration> serviceConfiguration);
+        public IConsoleAppHost Build();
+        public IConsoleAppHostBuilder ConfigureLogging(Action<ILoggingBuilder> configureLogging);
+        public IConsoleAppHostBuilder Configure(Action<IServiceProvider> configure);
+
+    }
+}
