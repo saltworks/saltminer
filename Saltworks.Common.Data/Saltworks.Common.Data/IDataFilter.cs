1f5d1a273e4ec9ed1380abb6e22ae92b01f1d289
bryant-sw <bryant@saltworks.io>
2025-03-26 09:11:49 -0500

    Initial Saltworks.Common.Data commit from ADO main branch.

---
+++ Saltworks.Common.Data/Saltworks.Common.Data/IDataFilter.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Saltworks.Common.Data
+{
+    public interface IDataFilter
+    {
+        bool AnyMatch { get; set; }
+        Dictionary<string, string> FilterMatches { get; }
+        string Index { get; set; }
+        Dictionary<string, bool> SortFilters { get; }
+    }
+}
---
+++ Saltworks.Common.Data/Saltworks.Common.Data/IDataRepositoryConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Saltworks.Common.Data
+{
+    public interface IDataRepositoryConfig
+    {
+        /// <summary>
+        /// Configuration key/value pairs
+        /// </summary>
+        Dictionary<string, string> Dictionary { get; }
+        /// <summary>
+        /// Configuration type mapping (i.e. this POCO belongs in that index/table)
+        /// </summary>
+        Dictionary<Type, string> Mappings { get; }
+    }
+}
