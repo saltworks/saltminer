096d5d5b2e6216fef481d3ca3cd64cb3d4981e9d
bryant-sw <bryant@saltworks.io>
2025-03-28 08:28:19 -0500

    Initial jobmanager commit from 3.0.8 branch

---
+++ Saltworks.SaltMiner.JobManager/Saltworks.SaltMiner.JobManager/Helpers/Extensions.cs
@@ -0,0 +1,164 @@
+﻿using Saltworks.SaltMiner.Core.Data;
+using Saltworks.SaltMiner.Core.Entities;
+
+namespace Saltworks.SaltMiner.JobManager.Helpers
+{
+    public static class Extensions
+    {
+        public static bool IsSameScanInfo(this Scan scan, QueueScan qscan)
+        {
+            var s1 = scan.Saltminer.Scan;
+            var s2 = qscan.Saltminer.Scan;
+
+            if (s1.ScanDate != s2.ScanDate)
+            {
+                return false;
+            }
+
+            if (s1.Product != s2.Product)
+            {
+                return false;
+            }
+
+            if (s1.ProductType != s2.ProductType)
+            {
+                return false;
+            }
+
+            if (s1.ReportId != s2.ReportId)
+            { 
+                return false; 
+            }
+
+            if (s1.Vendor != s2.Vendor)
+            {
+                return false; 
+            }
+
+            if (s1.Rulepacks.Count != s2.Rulepacks.Count)
+            { 
+                return false;
+            }
+
+            return true;
+        }
+
+        public static bool IsSameAssetInfo(this Asset asset, QueueAsset qasset)
+        {
+            var a1 = asset.Saltminer.Asset;
+            var a2 = qasset.Saltminer.Asset;
+
+            if (a1.Ip != a2.Ip)
+            {
+                return false;
+            }
+
+            if (a1.IsSaltminerSource != a2.IsSaltminerSource)
+            {
+                return false;
+            }
+
+            if (a1.Host != a2.Host)
+            {
+                return false;
+            }
+            
+            if (!(a1.Attributes?.IsDictionaryEqual(a2.Attributes) ?? false)) 
+            { 
+                return false; 
+            }
+            
+            if (a1.Description != a2.Description) 
+            { 
+                return false; 
+            }
+            
+            if (a1.IsProduction != a2.IsProduction) 
+            { 
+                return false; 
+            }
+            
+            if (a1.LastScanDaysPolicy != a2.LastScanDaysPolicy) 
+            { 
+                return false; 
+            }
+            
+            if (a1.IsRetired != a2.IsRetired) 
+            { 
+                return false; 
+            }
+            
+            if (a1.VersionId != a2.VersionId) 
+            { 
+                return false;
+            }
+            
+            if (a1.Port != a2.Port)
+            {
+                return false;
+            }
+
+            if (a1.Scheme != a2.Scheme)
+            {
+                return false;
+            }
+
+            if (a1.Name != a2.Name)
+            {
+                return false;
+            }
+            
+            return true;
+        }
+
+        public static bool IsDictionaryEqual<TKey, TValue>(this Dictionary<TKey, TValue> first, Dictionary<TKey, TValue> second)
+        {
+            if (first == null || second == null)
+            {
+                return first == null && second == null;
+            }
+
+            if (first.Count != second.Count)
+            {
+                return false;
+            }
+
+            foreach (var kv in first)
+                try
+                {
+                    if (!second.ContainsKey(kv.Key) || !kv.Value.Equals(second[kv.Key]))
+                    {
+                        return false;
+                    }
+                }
+                catch (ArgumentNullException)
+                {
+                    return false;
+                }
+                catch (NullReferenceException)
+                {
+                    return false;
+                }
+            return true;
+        }
+
+        public static SearchRequest NextRequest<T>(this SearchRequest request, DataResponse<T> response) where T: class
+        {
+            request.UIPagingInfo = response.UIPagingInfo;
+            request.AfterKeys = response.AfterKeys;
+
+            if (!string.IsNullOrEmpty(request.PitPagingInfo?.PagingToken))
+            {
+                request.PitPagingInfo = response.PitPagingInfo;
+            }
+            
+            if ((request.UIPagingInfo?.Page ?? 0) > 0)
+            {
+                request.UIPagingInfo.Page++;
+            }
+
+            return request;
+        }
+
+    }
+}
---
+++ Saltworks.SaltMiner.JobManager/Saltworks.SaltMiner.JobManager/Helpers/ReportFileName.cs
@@ -0,0 +1,94 @@
+﻿using Saltworks.SaltMiner.UiApiClient.ViewModels;
+using System.Text.RegularExpressions;
+
+namespace Saltworks.SaltMiner.JobManager.Helpers
+{
+    public static class ReportFileName
+    {
+        public static string GetReportName(string template, EngagementSummary engagement)
+        {
+            var fileName = template;
+            var regex = new Regex(@"\{[^}]*\}");
+            var results = regex.Matches(template).Cast<Match>().Select(c => c.Value).ToList();
+
+            foreach(var field in results)
+            {
+                var fieldValue = field.Replace("{","").Replace("}","");
+                string newField = string.Empty;
+
+                if (fieldValue.StartsWith("Attributes"))
+                {
+                    var property = fieldValue.Replace("Attributes.", "");
+                    newField = engagement.Attributes != null && engagement.Attributes.Count > 0
+                        ? (engagement.Attributes.FirstOrDefault(a => a.Name.Equals(property, StringComparison.OrdinalIgnoreCase)).Value ?? string.Empty) : string.Empty;
+                }
+                else if (fieldValue.StartsWith("dt|"))
+                {
+                    var format = fieldValue.Split("|");
+                    newField = DateTime.Now.ToString("yyyy-MM-dd");
+                    if (format.Length > 1)
+                    {
+                        newField = DateTime.Now.ToString(format[1]);
+                    }
+                }
+                else
+                {
+                    var type = engagement.GetType();
+                    var property = type.GetProperty(fieldValue);
+                    newField = property.GetValue(engagement, null).ToString();
+                }
+
+                fileName = fileName.Replace(field, SanitizeFilename(newField));
+            }
+
+            return fileName;
+        }
+
+        public static string GetReportName(string template, EngagementFull engagement)
+        {
+            var fileName = template;
+            var regex = new Regex(@"\{[^}]*\}");
+            var results = regex.Matches(template).Cast<Match>().Select(c => c.Value).ToList();
+
+            foreach (var field in results)
+            {
+                var fieldValue = field.Replace("{", "").Replace("}", "");
+                string newField = null;
+
+                if (fieldValue.StartsWith("Attributes"))
+                {
+                    var property = fieldValue.Replace("Attributes.", "");
+                    newField = engagement.Attributes != null && engagement.Attributes.Count > 0
+                        ? (engagement.Attributes.FirstOrDefault(a => a.Name.Equals(property, StringComparison.OrdinalIgnoreCase)).Value ?? string.Empty) : string.Empty;
+                }
+                else
+                {
+                    var type = engagement.GetType();
+                    var property = type.GetProperty(fieldValue);
+                    newField = property.GetValue(engagement, null).ToString();
+                }
+
+                fileName = fileName.Replace(field, newField);
+            }
+
+            return fileName;
+        }
+
+        private static string SanitizeFilename(string filename)
+        {
+            // Invalid file name characters
+            string pattern = "[<>:\"/\\\\|?*\\x00-\\x1F]";
+
+            // Replace invalid
+            string sanitizedFilename = Regex.Replace(filename, pattern, "-");
+
+            // Replace characters at the end if they are a space or period
+            if (sanitizedFilename.EndsWith(' ') || sanitizedFilename.EndsWith('.'))
+            {
+                sanitizedFilename = Regex.Replace(sanitizedFilename, "[ .]$", "-");
+            }
+
+            return sanitizedFilename;
+        }
+    }
+}
