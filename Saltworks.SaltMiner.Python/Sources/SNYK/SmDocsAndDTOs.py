1ca12325ffe328e14d20934119263fb394786e1b
cameron <cameron@saltworks.io>
2025-04-15 10:28:38 -0600

    Snyk Source addition

---
+++ Saltworks.SaltMiner.Python/Sources/SNYK/SmDocsAndDTOs.py
@@ -0,0 +1,254 @@
+import json 
+from typing import Dict, Any, Optional
+from datetime import datetime
+from pydantic import BaseModel
+class SnykDocs:
+    """
+    A class to handle all needed data structures for the SnykAdpater in the form of a function that returns 
+    a json dictionary
+    """
+
+    def __init__(self):
+        pass 
+
+    def map_asset_doc(self):
+        return {
+                "Saltminer": {
+                    "Asset": {  
+                        "Name": None,
+                        "Description": None,
+                        "VersionId": None,
+                        "Version": None,
+                        "ConfigName": None,
+                        "SourceType": None,
+                        "IsSaltMinerSource": True,
+                        "SourceId": None,
+                        "IsProduction": True, 
+                        "AssetType": None,
+                        "Instance": None,
+                        "Attributes": {},
+                        "LastScanDaysPolicy": "30"
+                    },
+                    "InventoryAsset": {
+                        "Key": ""
+                    },
+                    "Internal": {
+                        "QueueScanId": None
+                    }
+                },
+                "Timestamp": None
+            }
+    
+    def map_scan_doc(self):
+        return {
+            "Saltminer": {
+                "Internal": {
+                    "AgentId": None,
+                    "IssueCount": None,
+                    "CurrentQueueScanId": None,
+                    "ReplaceIssues": False
+                },
+                "Scan": {
+                    "AssessmentType": None,
+                    "ProductType": None,
+                    "Product": None,
+                    "Vendor": None,
+                    "ReportId": None,
+                    "ScanDate": None,
+                    "SourceType": None,
+                    "IsSaltMinerSource": True,
+                    "ConfigName":None,
+                    "AssetType": None,
+                    "Instance": None,
+                    "Rulepacks": []
+                }
+            },
+            "Timestamp": None
+        }
+    
+    def map_issue_doc(self):
+        return {
+                "Saltminer": {
+                    "QueueScanId": None,
+                    "QueueAssetId": None,
+                    "Source": {
+                        "Analyzer": None,
+                        "Confidence": None,
+                        "Impact": None,
+                        "IssueStatus": None,
+                        "Kingdom": None,
+                        "Likelihood": None,
+                    },
+                    "Attributes":{},
+                },
+                "Vulnerability": {
+                    "IsActive": True,
+                    "Audit": {
+                        "Audited": False,
+                        "Auditor": "",                     
+                        "LastAudit": None                  
+                    },
+                    "FoundDate": None,
+                    "Id": [],                            
+                    "IsFiltered": False,
+                    "IsRemoved": False,
+                    "IsSuppressed":False,
+                    "Location": None,
+                    "LocationFull": None,
+                    "RemovedDate": None,
+                    "SourceSeverity": None,
+                    "ReportId": None,
+                    "Category": [ "Application" ],         
+                    "Classification": "",                  
+                    "Description": "",                     
+                    "Enumeration": "",                     
+                    "Name": None,
+                    "Reference": None,
+                    "Severity": None,
+                    "Scanner": {
+                        "ApiUrl": None,
+                        "GuiUrl":None,
+                        "Id": None,
+                        "AssessmentType": None,
+                        "Product": None,
+                        "Vendor": None,
+                    },
+                    "Score": {
+                        "Base": 0, 
+                        "Environmental": 0,   
+                        "Temporal": 0,             
+                        "Version": None                                           
+                    }
+                },
+                "Labels": {},                               
+                "Message": None,                            
+                "Tags": None,                             
+                "Timestamp": None,
+            }
+
+
+#The DTO classes are used to process the Queue_Scan, Queue_Issue, and Queue_Asset json docs through a data validation process prior to sending them to Saltminer
+class BaseDocDTO(BaseModel):
+    '''
+    A base DTO to contain information shared across all data types in the adapter (Asset, Scan, Issue)
+    '''
+    Timestamp: Optional[datetime] = None
+
+#These are a collection of shared DTOs for Assets, Scans, and Issues.
+class Internal(BaseModel):
+    QueueScanId: Optional[str] = None
+    IssueCount: Optional[int] = -1
+
+class InventoryAsset(BaseModel):
+    Key: str = ""
+
+class Scanner(BaseModel):
+    ApiUrl: Optional[str] = None
+    GuiUrl: Optional[str] = None
+    Id: Optional[str] = None
+    AssessmentType: Optional[str] = None
+    Product: Optional[str] = None
+    Vendor: Optional[str] = None
+
+#These are a collection of DTOs specific to Assets
+class Asset(BaseModel):
+    Name: Optional[str] = None
+    Description: Optional[str] = None
+    VersionId: Optional[str] = None
+    Version: Optional[str] = None
+    ConfigName: Optional[str] = None
+    SourceType: Optional[str] = None
+    IsSaltMinerSource: bool = True
+    SourceId: Optional[str] = None
+    IsProduction: Optional[bool] = None
+    AssetType: Optional[str] = None
+    Instance: Optional[str] = None
+    Attributes: Dict[str, Any] = {}
+    LastScanDaysPolicy: str = "30"
+
+class SaltminerAsset(BaseModel):
+    Asset: Asset
+    InventoryAsset: InventoryAsset
+    Internal: Internal
+
+class MapAssetDocDTO(BaseDocDTO):
+    Saltminer: SaltminerAsset
+
+#These  are a collection of DTOs specific to Scans 
+class Scan(BaseModel):
+    AssessmentType: str = None
+    ProductType: str = None
+    Product: Optional[str] = None
+    Vendor: Optional[str] = None
+    ReportId: str = None
+    ScanDate: Optional[str] = None
+    SourceType: Optional[str] = None
+    IsSaltMinerSource: bool = True
+    ConfigName: Optional[str] = None
+    AssetType: str = None
+    Instance: Optional[str] = None
+    Rulepacks: list[str] = []
+
+
+class SaltminerScan(BaseModel):
+    Internal: Dict[str, Any]
+    Scan: Scan
+
+
+class MapScanDocDTO(BaseDocDTO):
+    Saltminer: SaltminerScan
+
+#These are a collection of DTOs specific to Issues 
+class Source(BaseModel):
+    Analyzer: Optional[str] = None
+    Confidence: Optional[str] = None
+    Impact: Optional[str] = None
+    IssueStatus: Optional[str] = None
+    Kingdom: Optional[str] = None
+    Likelihood: Optional[str] = None
+
+
+class Score(BaseModel):
+    Base: Optional[float] = None
+    Environmental: Optional[float] = None
+    Temporal: Optional[float] = None
+    Version: Optional[str] = None
+
+
+class Vulnerability(BaseModel):
+    IsActive: Optional[bool] = None
+    Audit: Dict[str, Any] = {}
+    FoundDate: Optional[str] = None
+    Id: list[str] = None
+    IsFiltered: Optional[bool] = None
+    IsRemoved: bool
+    IsSuppressed: Optional[bool] = None
+    Location: Optional[str] = None
+    LocationFull: Optional[str] = None
+    RemovedDate: Optional[str] = None
+    SourceSeverity: Optional[str] = None
+    ReportId: Optional[str] = None
+    Category: list[str] = ["Application"]
+    Classification: str = ""
+    Description: str = ""
+    Enumeration: str = ""
+    Name: Optional[str] = None
+    Reference: Optional[str] = None
+    Severity: Optional[str] = None
+    Scanner: Scanner
+    Score: Score
+
+
+class SaltminerIssue(BaseModel):
+    QueueScanId: Optional[str] = None
+    QueueAssetId: Optional[str] = None
+    Source: Source
+    Attributes: Dict[str, Any] = {}
+
+
+class MapIssueDocDTO(BaseDocDTO):
+    Saltminer: SaltminerIssue
+    Vulnerability: Vulnerability
+    Labels: Dict[str, Any] = {}
+    Message: Optional[str] = None
+    Tags: Optional[list[str]] = None
---
+++ Saltworks.SaltMiner.Python/Sources/SNYK/SnykAdapter.py
@@ -0,0 +1,255 @@
+import json
+import logging 
+import time
+from datetime import datetime, timezone, timedelta
+
+from Sources.SNYK.SnykClient import SnykClient
+from Sources.SNYK.SmDocsAndDTOs import SnykDocs, MapAssetDocDTO, MapIssueDocDTO, MapScanDocDTO
+
+from Core.SmDataClient import SmDataClient
+from Core.ElasticClient import ElasticClient
+
+class SnykAdapter:
+    """
+    Handles fetching and syncing issues, scans, and assets from Snyk to Saltminer.
+
+    Process Summary:
+    - Pull `snyk_last_updated` timestamps from Saltminer to determine what needs syncing.
+    - Iterate through Snyk projects and retrieve associated issues if updated.
+    - For updated projects:
+        • Queue the Scan
+        • Queue the Asset (linked to the Scan)
+        • Queue the Issues (linked to the Scan and Asset)
+    - Finalize the scan to trigger Saltminer’s ingestion of queued data.
+
+    For detailed process flow and Saltminer integration logic, see the README.
+    """
+    def __init__(self, settings):
+        self.snyk_client = SnykClient(settings)
+        self.snyk_docs = SnykDocs()
+        self._es = ElasticClient(settings)
+        self._sm_data_client = SmDataClient(settings, "Snyk")
+        self.prj_version_last_updated = {}
+
+
+    def run_sync(self, first_load= False):
+        """
+        This is used to call the needed functions for the sync process
+        """
+        if first_load:
+            self.get_prj_last_updated()
+
+        self.get_sync(first_load=first_load)
+
+
+    def get_sync(self, first_load= False):  
+        """
+        Fetches all data from the Snyk client by calling necessary API requests.
+        - Retrieves projects.
+        - Checks if issues exist for each project before processing.
+        - Runs sync_issues if they exist
+        """
+        start_date = None
+        for project in self.snyk_client.get_snyk_projects_generator():
+            
+            project_id = project.get("id") # Ensure project ID is extracted properly
+            if not project_id:
+                logging.warning("Skipping project with missing ID: %s", project)
+                continue
+
+            if not first_load and project_id in self.prj_version_last_updated.keys() and self.prj_version_last_updated.get(project_id):
+                date = datetime.strptime(self.prj_version_last_updated.get(project_id), "%Y-%m-%dT%H:%M:%S.%fZ")
+                start_date = date.strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3] + "Z"
+
+            issues_generator = self.snyk_client.get_sync_issues_generator(limit=100, project_id=project_id, start_date=start_date)
+            first_issue = next(issues_generator, None) # Get first issue to check if there is any data
+
+            if first_issue:
+                self.snyc_issues(project, first_issue, issues_generator)
+                
+            else:
+                logging.info("No issues found for project %s, skipping.", project_id)
+
+
+    def snyc_issues(self, project, first_issue, issues_generator):
+        """
+        Runs through issues of a specific project and sends them to Saltminer.
+        -Maps Scan to SM valid scan document
+        -Sends mapped scan to SM queueScans
+        -Maps Asset to SM valid asset document
+        -Sends mapped asset to SM queueAssets
+        -Maps all issues to SM valid issue documents
+        -Sends mapped issues to SM queueIssues
+        -Runs FinalizeQueue to set all queue document's queue status to 'Pending' so SM will process all the queued data we just sent in. 
+        """
+        project_id = project.get("id") # Ensure project ID is extracted properly
+        counter = 0 
+        logging.info("Issues found for project %s, running mapAsset() and mapScan()", project_id)
+        # Run mapping functions
+        try:
+            #Maps Scan to SM valid scan document
+            mapped_scan = self.map_scan(project)
+            #Sends mapped scan to SM queue_scans
+            queue_scan = self._sm_data_client.AddQueueScan(json.loads(mapped_scan.model_dump_json()))
+            #Maps Asset to SM valid asset document
+            mapped_asset = self.map_asset(project, queue_scan['id'])
+            #Sends mapped asset to SM queue_assets
+            queue_asset = self._sm_data_client.AddQueueAsset(json.loads(mapped_asset.model_dump_json()))
+
+            # Process the first issue
+            mapped_issue = self.map_issue(first_issue, queue_scan['id'], queue_asset['id'], queue_scan['saltminer']['scan']['reportId'], project_id)
+            #Send the first issue to SM queue_issues
+            self._sm_data_client.AddQueueIssue(json.loads(mapped_issue.model_dump_json()))
+            counter += 1 
+
+            #Maps all issues to SM valid issue documents
+            for issue in issues_generator:
+                mapped_issue = self.map_issue(issue, queue_scan['id'], queue_asset['id'], queue_scan['saltminer']['scan']['reportId'], project_id)
+                #Sends mapped issues to SM queue_issues
+                self._sm_data_client.AddQueueIssue(json.loads(mapped_issue.model_dump_json()))
+                #Tracking the counter so that we can use it to add to the Scan.Issue_count in the future.
+                counter += 1
+            #TODO:TEST SENDALLBATCHISSUESf
+            self._sm_data_client.SendAllBatchIssues()
+            self._sm_data_client.FinalizeQueue(queue_scan['id'])
+            logging.info("[SyncAdapter][Sync Issues] FinalizeQueue run for project id : %s, number of issues sent: %s", project_id, counter)
+
+        except Exception as e:
+            logging.error("[SnykAdapter][Sync Issues] An exception occurred during issue processing: %s", e)
+
+
+    def get_prj_last_updated(self):
+        """
+        This is going to call Saltminer to get the last updated date of all the Snyk project versions currently present in Saltminer.
+        """
+        if self._es.IndexExists('issues_app_saltworks.snyk_snyk1'):
+            search = self._es.Search(index ='issues_app_saltworks.snyk_snyk1', queryBody=self.last_updated_query(), size=10000, navToData=False)
+            for item in search['aggregations']['version_id']['buckets']:
+                self.prj_version_last_updated[item['key']] = item['snyk_last_updated'].get('value_as_string')
+        else:
+            return None
+
+
+    def map_issue(self, issue, queue_scan_id, queue_asset_id, report_id, project_id):
+        """
+        This will map the issue data into the needed format for the queue_issues
+        """
+        q_issue_doc = self.snyk_docs.map_issue_doc()
+        q_issue_doc['Timestamp'] = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3] + "Z"
+        saltminer = q_issue_doc['Saltminer']
+        saltminer['Attributes']['snyk_last_updated'] = issue['attributes']['updated_at']
+        saltminer['Attributes']['status'] = issue['attributes']['status']
+        saltminer['QueueScanId'] = queue_scan_id
+        saltminer['QueueAssetId']= queue_asset_id
+        vulnerability = q_issue_doc['Vulnerability']
+
+        #Setting the removed date will trigger the IsRemoved boolean to change within SM
+        if issue['attributes']['status'] == 'resolved':
+            if issue['attributes'].get('resolution') and issue['attributes']['resolution'].get('resolved_at'):
+                vulnerability['RemovedDate'] = issue['attributes']['resolution'].get('resolved_at')
+
+
+        vulnerability['FoundDate'] = issue['attributes']['created_at']
+        vulnerability["Id"] = [problem['id'] for problem in issue['attributes']['problems']]
+        if issue["attributes"].get('classes'):
+            vulnerability["Id"].extend([cls['id'] for cls in issue['attributes']['classes']])
+        vulnerability['Severity'] = issue['attributes']['effective_severity_level'].title()
+        vulnerability['Name'] = issue['attributes']['title']
+        vulnerability['LocationFull'] = "None"
+        vulnerability['Location'] = "None"
+        vulnerability['ReportId'] = report_id
+
+        scanner = vulnerability['Scanner']
+        scanner['Id'] = issue['id'] + "|" + project_id
+        #TODO:DETERMINE THE CORRECT SCAN TYPES AND PUT THEM HERE 
+        scanner['AssessmentType'] = "SAST"
+        #scanner['AssessmentType'] = self.get_assessment_type(issue['attributes']['type']) 
+        #Location_full???
+        scanner['Product'] = "Snyk"
+        scanner['Vendor']= "Snyk"
+
+        return MapIssueDocDTO(**q_issue_doc)
+
+
+    def map_scan(self, project):
+        """
+        This will map a scan into hte format needed for queue issue import
+        """
+        q_scan_doc = self.snyk_docs.map_scan_doc()
+        q_scan_doc['Timestamp'] = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3] + "Z"
+        q_scan_doc['Saltminer']['Internal']['IssueCount'] = -1  #Setting this value to -1 disables IssueCount validation
+        scan = q_scan_doc['Saltminer']['Scan']
+        scan['Product'] = "Snyk"
+        scan['Vendor']= "Snyk"
+        scan['ReportId'] = project['id'] + project['attributes']['name'] + "|" + datetime.now().isoformat()
+        scan['ScanDate'] = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3] + "Z"
+        scan['SourceType'] = "Saltworks.Snyk"
+        scan['Instance'] = "Snyk1"
+        scan['AssetType'] = "app"
+        scan['AssessmentType'] = "SAST"
+        scan['ProductType'] = "Application"
+
+        return MapScanDocDTO(**q_scan_doc)
+
+
+    def map_asset(self, project, q_scan_id):
+        """
+        This will map an asset into the format needed for queue issue import
+        """
+        split_project_path = project['attributes']['name'].split(':')
+        q_asset_doc = self.snyk_docs.map_asset_doc()
+        q_asset_doc['Timestamp'] = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3] + "Z"
+        q_asset_doc['Saltminer']['Internal']['QueueScanId'] = q_scan_id
+        asset = q_asset_doc['Saltminer']['Asset']
+        asset['Name'] = project['attributes']['name']
+        asset['Version']= split_project_path[-1]
+        asset['VersionId'] = project['id']
+        asset['Instance'] = "Snyk1"
+        asset['SourceId'] = project['relationships']['target']['data']['id'] if project['relationships'].get('target') else split_project_path[-1]
+        asset['AssetType']= "app"
+        asset['SourceType'] = "Saltworks.Snyk"
+
+        return MapAssetDocDTO(**q_asset_doc)
+    
+
+    def get_assessment_type(self, source_assessment_type):
+        assessment_types = {
+            "package_vulnerability": "SAST"
+
+        }
+
+        return assessment_types.get(source_assessment_type)
+
+
+    def get_issue_counts(self, issue_counts_object):
+        counter = 0 
+        for item in issue_counts_object.keys():
+            if item in ["critical", "high", "medium"," low"]:
+                counter += issue_counts_object[item]
+
+        return counter
+
+
+    def last_updated_query(self):
+        return {
+            "aggs": {
+                "version_id": {
+                    "terms": {
+                        "field": "saltminer.asset.version_id",
+                        "order": {
+                            "_key": "desc"
+                        },
+                        "size": 10000
+                    },
+                    "aggs": {
+                        "snyk_last_updated": {
+                            "max": {
+                                "field": "saltminer.attributes.snyk_last_updated"
+                            }
+                        }
+                    }
+                }
+            },
+            "size": 0
+        }
+
