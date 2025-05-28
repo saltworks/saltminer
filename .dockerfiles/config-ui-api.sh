e259c538f8fc7ff5c8040b752d8c121c14330ac1
bryant-sw <bryant@saltworks.io>
2025-05-28 15:46:49 -0500

    UI API simplified config

---
+++ .dockerfiles/config-ui-api.sh
@@ -0,0 +1,9 @@
+#!/bin/ash
+handle_error() {
+  echo "Error on line $1"
+  echo "This does not affect SaltMiner container start."
+  # Errors shouldn't prevent other containers from starting.
+  exit 0
+}
+
+trap 'handle_error $LINENO' ERR
