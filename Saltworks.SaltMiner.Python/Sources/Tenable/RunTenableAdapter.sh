f8a3ee1d6ca962a140f4a7f354bcf6afc06caf17
cameron <cameron@saltworks.io>
2025-06-05 16:19:57 -0600

    Bugfix

--- Saltworks.SaltMiner.Python/Sources/Tenable/RunTenableAdapter.sh
+++ Saltworks.SaltMiner.Python/Sources/Tenable/RunTenableAdapter.sh
@@ -2,5 +2,5 @@
 cd /usr/share/saltworks/saltminer-2.5.0
 export SALTMINER_2_CONFIG_PATH=/etc/saltworks/saltminer-2.5.0
 
-python3 -m Sources.SNYK.RunTenableAdapter 
+python3 -m Sources.Tenable.RunTenableAdapter 
 
