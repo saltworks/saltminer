9728325be7fab486787e259b119175600f93debc
tdsalty <51519226+tdsalty@users.noreply.github.com>
2025-09-23 07:09:09 -0500

    bugfix checkout

--- Saltworks.SaltMiner.Ui.Api/Saltworks.SaltMiner.Ui.Api/Contexts/EngagementContext.cs
+++ Saltworks.SaltMiner.Ui.Api/Saltworks.SaltMiner.Ui.Api/Contexts/EngagementContext.cs
@@ -497,7 +497,7 @@ namespace Saltworks.SaltMiner.Ui.Api.Contexts
                     assetBatch = [];
                 }
 
-                var engagementIssues = fullEngagement.Issues.Where(x => x.AssetId == asset.AssetId && x.IsRemoved);
+                var engagementIssues = fullEngagement.Issues.Where(x => x.AssetId == asset.AssetId && !x.IsRemoved);
                 if (Config.EngagementCheckoutIncludesClosedIssues)
                     engagementIssues = fullEngagement.Issues.Where(x => x.AssetId == asset.AssetId);
                 if (Config.EngagementCheckoutOnlyActiveIssues)
