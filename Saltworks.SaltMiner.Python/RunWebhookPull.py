705c1e4758db7f1284f88da84d005249a1bff102
tdsalty <51519226+tdsalty@users.noreply.github.com>
2025-11-10 13:14:37 -0600

    add main loop to webhook puller, don't just get one batch

--- Saltworks.SaltMiner.Python/RunWebhookPull.py
+++ Saltworks.SaltMiner.Python/RunWebhookPull.py
@@ -49,41 +49,48 @@ sqh = SyncQueueHelper(app.Settings, prmSourceName)
 
 # Let's go
 sscIds = []
-data = api.GetWebhookEvents(prmWebhookSource)
-if not data:
-    logging.info("[Webhook Pull] No webhook data returned.")
-    sys.exit(0)
+foundSome = False
+maxLoops = 500
+curLoop = 1
+while curLoop < maxLoops:
+    data = api.GetWebhookEvents(prmWebhookSource)
+    if not data:
+        if not foundSome:
+            logging.info("[Webhook Pull] No webhook data returned.  Processing complete")
+        logging.info("[Webhook Pull] No more webhook data found.")
+        sys.exit(0)
+    foundSome = True
 
-logging.info("[Webhook Pull] Processing %s webhook (queue sync) items.", len(data))
-count = 0
-for dataItm in data:
-    didSomething = False
-    if not dataItm['payload']:
-        logging.error("[Webhook Pull] Invalid/missing payload for webhook (queue sync item) ID %s, skipping.", dataItm['id'])
-        continue
-    payload = json.loads(dataItm['payload'])
+    logging.info("[Webhook Pull] Processing %s webhook (queue sync) items.", len(data))
+    count = 0
+    for dataItm in data:
+        didSomething = False
+        if not dataItm['payload']:
+            logging.error("[Webhook Pull] Invalid/missing payload for webhook (queue sync item) ID %s, skipping.", dataItm['id'])
+            continue
+        payload = json.loads(dataItm['payload'])
 
-    # SSC
-    if 'events' in payload and payload['events'] and 'projectVersionId' in payload['events'][0]:
-        didSomething = True
-        for evt in payload['events']:
-            if 'projectVersionId' in evt:
-                event = "?" if not 'event' in evt else evt['event']
-                user = "?" if not 'username' in evt else evt['username']
-                logging.info("[Webhook Pull] SSC update event '%s' found for project version %s, tagged with username %s.", event, evt['projectVersionId'], user)
-                sscIds.append(evt['projectVersionId'])
-            else:
-                logging.error("[Webhook Pull] SSC update event may be malformed (missing projectVersionId), encountered in webhook (queue sync item) ID %s. Skipping.", dataItm['id'])
+        # SSC
+        if 'events' in payload and payload['events'] and 'projectVersionId' in payload['events'][0]:
+            didSomething = True
+            for evt in payload['events']:
+                if 'projectVersionId' in evt:
+                    event = "?" if not 'event' in evt else evt['event']
+                    user = "?" if not 'username' in evt else evt['username']
+                    logging.info("[Webhook Pull] SSC update event '%s' found for project version %s, tagged with username %s.", event, evt['projectVersionId'], user)
+                    sscIds.append(evt['projectVersionId'])
+                else:
+                    logging.error("[Webhook Pull] SSC update event may be malformed (missing projectVersionId), encountered in webhook (queue sync item) ID %s. Skipping.", dataItm['id'])
 
-    if not didSomething:
-        logging.info("[Webhook Pull] Unknown type of webhook (queue sync item) with ID '%s'.", dataItm['id'])
+        if not didSomething:
+            logging.info("[Webhook Pull] Unknown type of webhook (queue sync item) with ID '%s'.", dataItm['id'])
 
-    count += 1
-    if count % 100 == 0:
-        logging.info("[Webhook Pull] %s/%s webhook (queue sync) items processed.", count, len(data))
+        count += 1
+        if count % 100 == 0:
+            logging.info("[Webhook Pull] %s/%s webhook (queue sync) items processed.", count, len(data))
 
-if len(sscIds) > 0:
-    logging.info("[Webhook Pull] %s total SSC IDs to queue for updates.", len(sscIds))
-    sqh.InsertQueueBatch(sscIds, force=True)
-
-logging.info("[Webhook Pull] Processing complete.")
+    if len(sscIds) > 0:
+        logging.info("[Webhook Pull] %s total SSC IDs to queue for updates.", len(sscIds))
+        sqh.InsertQueueBatch(sscIds, force=True)
+    curLoop += 1
+logging.warning("[Webhook Pull] Max data loops occurred (%s), stopping processing.", maxLoops)
