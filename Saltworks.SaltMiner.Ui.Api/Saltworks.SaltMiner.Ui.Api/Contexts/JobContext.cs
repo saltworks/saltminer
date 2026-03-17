431a0fd941ee7a7a0d46671051273bd21aba7599
tdsalty <51519226+tdsalty@users.noreply.github.com>
2026-03-17 17:54:10 -0500

    service manager related tweak

--- Saltworks.SaltMiner.Ui.Api/Saltworks.SaltMiner.Ui.Api/Contexts/JobContext.cs
+++ Saltworks.SaltMiner.Ui.Api/Saltworks.SaltMiner.Ui.Api/Contexts/JobContext.cs
@@ -14,76 +14,75 @@
 * ----
 */
 
-﻿using Saltworks.SaltMiner.Core.Data;
+using Saltworks.SaltMiner.Core.Data;
 using Saltworks.SaltMiner.Core.Entities;
 using Saltworks.SaltMiner.Ui.Api.Models;
 using Saltworks.SaltMiner.UiApiClient.Helpers;
 using Saltworks.SaltMiner.UiApiClient.Requests;
 using Saltworks.SaltMiner.UiApiClient.Responses;
 
-namespace Saltworks.SaltMiner.Ui.Api.Contexts
+namespace Saltworks.SaltMiner.Ui.Api.Contexts;
+public class JobContext(IServiceProvider services, ILogger<JobContext> logger) : ContextBase(services, logger)
 {
-    public class JobContext(IServiceProvider services, ILogger<JobContext> logger) : ContextBase(services, logger)
+    public UiNoDataResponse DeleteReport(string id)
     {
-        public UiNoDataResponse DeleteReport(string id)
-        {
-            General.ValidateIdAndInput(id, Config.ApiFieldRegex, "id");
-            DataClient.JobDelete(id);
-            return new UiNoDataResponse(1);
-        }
+        General.ValidateIdAndInput(id, Config.ApiFieldRegex, "id");
+        DataClient.JobDelete(id);
+        return new UiNoDataResponse(1);
+    }
 
-        public UiDataResponse<Job> PullPendingJobs(UiPager paging = null, string type = null)
+    public UiDataResponse<Job> PullPendingJobs(UiPager paging = null, string type = null)
+    {
+        var request = new SearchRequest
         {
-            var request = new SearchRequest
+            PagingInfo = paging != null ? paging.ToPagingInfo() : new(10),
+            Filter = new Filter
             {
-                PagingInfo = paging != null ? paging.ToPagingInfo() : new(10),
-                Filter = new Filter
+                FilterMatches = new Dictionary<string, string>
                 {
-                    FilterMatches = new Dictionary<string, string>
-                    {
-                        { "Status", Job.JobStatus.Pending.ToString("g") }
-                    }
-                },
-                SortKeys = new() { { "Timestamp", false } }
-            };
+                    { "Status", Job.JobStatus.Pending.ToString("g") }
+                }
+            },
+            SortKeys = new() { { "Timestamp", false } }
+        };
 
-            if (!string.IsNullOrEmpty(type))
-                request.Filter.FilterMatches.Add("Type", type);
-            
-            var result = DataClient.JobSearch(request);
-            return new UiDataResponse<Job>(result.Data, result.PagingInfo);
-        }
+        if (!string.IsNullOrEmpty(type))
+            request.Filter.FilterMatches.Add("Type", type);
+        
+        var result = DataClient.JobSearch(request);
+        return new UiDataResponse<Job>(result.Data, result.PagingInfo);
+    }
 
-        public UiDataItemResponse<Job> UpdateQueue(Job queue, KibanaUser user)
+    public UiDataItemResponse<Job> UpdateQueue(Job queue, KibanaUser user)
+    {
+        queue.User ??= user.UserName;
+        queue.UserFullName ??= user.FullName;
+        queue.Status = Job.JobStatus.Pending.ToString("g");  // tell job manager there's an update to process
+
+        var result = DataClient.JobAddUpdate(queue);
+
+        DataClient.RefreshIndex(Job.GenerateIndex());
+
+        return new UiDataItemResponse<Job>(result.Data);
+    }
+
+    public UiDataResponse<Job> GetJobs(JobSearch searchRequest, bool isFinished = false)
+    {
+        var request = new SearchRequest
         {
-            queue.User ??= user.UserName;
-            queue.UserFullName ??= user.FullName;
+            PagingInfo = searchRequest.Pager?.ToPagingInfo() ?? new(10),
+            SortKeys = new() { { "timestamp", false } },
+            Filter = new Filter { AnyMatch = true }
+        };
+        List<string> terms = isFinished ? 
+            [Job.JobStatus.Complete.ToString("g"), Job.JobStatus.Error.ToString("g")] :
+            [Job.JobStatus.Pending.ToString("g"), Job.JobStatus.Processing.ToString("g")];
+        request.Filter.AddTermsFilterMatch("Status", terms);
 
-            var result = DataClient.JobAddUpdate(queue);
+        if (!string.IsNullOrEmpty(searchRequest.Type))
+            request.Filter.FilterMatches.Add("Type", searchRequest.Type);
 
-            DataClient.RefreshIndex(Job.GenerateIndex());
-
-            return new UiDataItemResponse<Job>(result.Data);
-        }
-
-        public UiDataResponse<Job> GetJobs(JobSearch searchRequest, bool isFinished = false)
-        {
-            var request = new SearchRequest
-            {
-                PagingInfo = searchRequest.Pager?.ToPagingInfo() ?? new(10),
-                SortKeys = new() { { "timestamp", false } },
-                Filter = new Filter { AnyMatch = true }
-            };
-            List<string> terms = isFinished ? 
-                [Job.JobStatus.Complete.ToString("g"), Job.JobStatus.Error.ToString("g")] :
-                [Job.JobStatus.Pending.ToString("g"), Job.JobStatus.Processing.ToString("g")];
-            request.Filter.AddTermsFilterMatch("Status", terms);
-
-            if (!string.IsNullOrEmpty(searchRequest.Type))
-                request.Filter.FilterMatches.Add("Type", searchRequest.Type);
-
-            var response = DataClient.JobSearch(request);
-            return new UiDataResponse<Job>(response.Data, response.PagingInfo);
-        }
+        var response = DataClient.JobSearch(request);
+        return new UiDataResponse<Job>(response.Data, response.PagingInfo);
     }
 }
