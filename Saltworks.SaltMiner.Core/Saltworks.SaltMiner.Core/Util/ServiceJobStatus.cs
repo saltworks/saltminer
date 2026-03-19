4639253b93254fcc87c42fb78668b26918fc2fce
tdsalty <51519226+tdsalty@users.noreply.github.com>
2026-03-19 10:42:56 -0500

    update license block

--- Saltworks.SaltMiner.Core/Saltworks.SaltMiner.Core/Util/ServiceJobStatus.cs
+++ Saltworks.SaltMiner.Core/Saltworks.SaltMiner.Core/Util/ServiceJobStatus.cs
@@ -1,15 +1,19 @@
 /* --[auto-generated, do not modify this block]--
 *
-* Copyright (c) 2025 Saltworks Security, LLC
+* SaltMiner - The open source vulnerability and pen testing management platform
+* Copyright (C) 2024-2026 Saltworks Security, LLC
 *
-* Use of this software is governed by the Business Source License included
-* in the LICENSE file.
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License.
 *
-* Change Date: 2029-12-09
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
 *
-* On the date above, in accordance with the Business Source License, use
-* of this software will be governed by version 2 or later of the General
-* Public License.
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <https://www.gnu.org/licenses/>.
 *
 * ----
 */
