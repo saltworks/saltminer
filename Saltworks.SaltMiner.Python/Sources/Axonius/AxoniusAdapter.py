49a7f256c1f7a22e1d96f36ec1cdd7ffebdbf708
cameron <cameron@saltworks.io>
2026-03-19 14:06:16 -0600

    Adpater testing changes

--- Saltworks.SaltMiner.Python/Sources/Axonius/AxoniusAdapter.py
+++ Saltworks.SaltMiner.Python/Sources/Axonius/AxoniusAdapter.py
@@ -18,20 +18,44 @@ class AxoniusAdapter:
         self._sm_data_client = SmDataClient(settings, "Axonius")
         self._es = ElasticClient(settings)
         self.last_updated_dict = {}
-        self.queue_scan_id_list = []
+        self.found_date_dict = {} 
+        self.queue_scan_id_dict = {}
+        self.queue_asset_id_dict = {}
+        #self.queue_scan_id_list = []
+        self.test_scanner_id = []
 
 
     def run_sync(self):
         self.get_last_updated()
-
+        self.get_found_dates()
+        counter = 0
         for asset in self.axonius_client.get_asset_query_generator(query_id= "69b42afef8e2bcfbf5f2bfcb", asset_type= "application_settings"):
-            print(asset.get("specific_data.data.product_name", []))
+            self.process_asset(asset, query_id= "69b42afef8e2bcfbf5f2bfcb")
+            counter += 1 
+        
+        self._sm_data_client.SendAllBatchIssues()
+        for key, value in self.queue_scan_id_dict.items():
+            self._sm_data_client.FinalizeQueue(value)
+            logging.info("[Axonius Adapter] Finalized QueueScan with id: %s", value)
+        print(counter)
+
+            
 
 
-    def process_assets(self, assets):
-        pass
+    def process_asset(self, asset, query_id):
+        asset_name =  ", ".join([item for item in asset.get("specific_data.data.product_name", [])])
+        if asset_name not in self.queue_scan_id_dict.keys():
+            mapped_scan = self.map_scan(asset, query_id=query_id)  
+            queue_scan = self._sm_data_client.AddQueueScan(json.loads(mapped_scan.model_dump_json()))
 
+            self.queue_scan_id_dict[asset_name] = queue_scan['id']
+            mapped_asset= self.map_asset(asset, queue_scan_id=queue_scan['id'])
+            queue_asset = self._sm_data_client.AddQueueAsset(json.loads(mapped_asset.model_dump_json()))
+            self.queue_asset_id_dict[asset_name] = queue_asset['id']
+        mapped_issue = self.map_issue(asset, queue_asset_id=self.queue_asset_id_dict[asset_name], queue_scan_id=self.queue_scan_id_dict[asset_name], query_id=query_id)
+        self._sm_data_client.AddQueueIssue(json.loads(mapped_issue.model_dump_json()))
 
+            
     def map_scan(self, asset, query_id):
         q_scan_doc = self.sm_docs.map_scan_doc()
         q_scan_doc['Timestamp'] = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3] + "Z"
@@ -56,24 +80,84 @@ class AxoniusAdapter:
         q_asset_doc = self.sm_docs.map_asset_doc()
         q_asset_doc['Timestamp'] = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3] + "Z"
         q_asset_doc['Saltminer']['Internal']['QueueScanId'] = queue_scan_id
-        asset = q_asset_doc['Saltminer']['Asset']
-        asset['Name'] = ", ".join([item for item in asset.get("specific_data.data.product_name", [])])
-        asset['AssetId'] = asset.get("internal_axon_id", "")
-        asset['AssetType'] = 'app'
-        asset['SourceType'] = "Saltworks.Axonius"
-        asset['Instance'] = "Axonius1"
-        asset['Version'] =  ", ".join([item for item in asset.get("specific_data.data.product_name", [])])
-        asset['VersionId'] =  ", ".join([item for item in asset.get("specific_data.data.product_name", [])])
+        asset_doc = q_asset_doc['Saltminer']['Asset']
+        product_name = asset.get("specific_data.data.product_name")
+
+        if isinstance(product_name, str):
+            product_name = [product_name]
+        elif not product_name:
+            product_name = []
+
+        joined_product_name = ", ".join(product_name) if product_name else "Unknown"
+        asset_doc['Name'] = joined_product_name
+        asset_doc['AssetId'] = self.get_first(asset, "internal_axon_id", "")
+            
+        asset_doc['AssetType'] = 'app'
+        asset_doc['SourceType'] = "Saltworks.Axonius"
+        asset_doc['Instance'] = "Axonius1"
+        asset_doc['Version'] =  joined_product_name
+        asset_doc['VersionId'] =  joined_product_name
+        asset_doc['SourceId'] =  joined_product_name
         q_asset_doc = self.map_asset_attributes(asset, q_asset_doc)
         return MapAssetDocDTO(**q_asset_doc)
 
     def map_asset_attributes(self, asset, q_asset_doc):
         modified_q_asset_doc = q_asset_doc
+        asset = modified_q_asset_doc['Saltminer']['Asset']
+        attributes = asset.get('Attributes', {})
+
+        attributes['RawSettingName'] = self.get_first(asset, "specific_data.data.raw_setting_name", "")
+        attributes['MetaDataClientUsed'] = self.get_first(asset, "meta_data.client_used", "")
+        attributes['Labels'] = ", ".join(asset.get("labels", []))
+        attributes['SettingName'] = self.get_first(asset, "specific_data.data.setting_name", "")
+        attributes['SettingNameDetails'] = self.get_first(asset, "specific_data.data.setting_name_details", "")
+        attributes['SettingsStatus'] = self.get_first(asset, "specific_data.data.settings_status", "")
+
         return modified_q_asset_doc
 
+    def map_issue(self, asset, queue_asset_id, queue_scan_id, query_id):
+        q_issue_doc = self.sm_docs.map_issue_doc()
+        q_issue_doc['Timestamp'] = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3] + "Z"
+        saltminer = q_issue_doc['Saltminer']
+        saltminer['QueueScanId'] = queue_scan_id
+        saltminer['QueueAssetId']= queue_asset_id
+        saltminer['IssueType'] = self.get_first(asset, "specific_data.data.settings_status", "")
+        vulnerability = q_issue_doc['Vulnerability']
+        finding_id = None
+        for label in asset.get("labels", []):
+            if 'finding' in label.lower():
+                finding_id = label
+                break
+        vulnerability['Id'] = [finding_id if finding_id else "None"]
+        vulnerability['Name'] = self.get_first(asset, "specific_data.data.raw_setting_name", "")
+        scanner_id = self.get_first(asset, "internal_axon_id", "") + "|" + finding_id if finding_id else self.get_first(asset, "internal_axon_id", "")
+        vulnerability['FoundDate'] = self.found_date_dict.get(scanner_id, datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3] + "Z")
+        vulnerability['ReportId'] = ", ".join([item for item in asset.get("specific_data.data.product_name", [])]) + "|" + query_id + datetime.now().isoformat()
+        vulnerability['recommendation']= self.get_first(asset, "specific_data.data.recommendation_description", "")
+        vulnerability['Details']= self.get_first(asset, "specific_data.data.raw_setting_name_details", "")
+        vulnerability['Location']= self.get_first(asset, "specific_data.data.product_name", "")
+        vulnerability['LocationFull'] = self.get_first(asset, "specific_data.data.product_name", "")
+        vulnerability_severity = self.get_first(asset, "specific_data.data.impact", "")
+        if vulnerability_severity not in ["Low", "Medium", "High", "Critical"]:
+            vulnerability_severity = 'Info'
+        vulnerability['Severity'] = vulnerability_severity
+        scanner = vulnerability['Scanner']
+        scanner['Product'] = "Axonius"
+        scanner['Vendor'] = "Axonius"
+        scanner['AssessmentType'] = "Open"
+        scanner['Id'] = scanner_id
 
-    def map_issue(self, issue, queue_asset_id, queue_scan_id, report_id):
-        pass
+
+        return MapIssueDocDTO(**q_issue_doc)
+        
+
+
+
+    def get_first(self, asset, key, default=""):
+        value = asset.get(key, default)
+        if isinstance(value, list) and len(value) > 0:
+            return value[0]
+        return value
 
     def get_last_updated(self):
         """
@@ -85,6 +169,13 @@ class AxoniusAdapter:
                 self.last_updated_dict[item['key']] = item['last_updated'].get('value_as_string')
         else:
             return None
+        
+
+    def get_found_dates(self):
+        if self._es.IndexExists('issues_app_saltworks.axonius_axonius1'):
+            search = self._es.Search(index ='issues_app_saltworks.axonius_axonius1', size=10000, navToData=False)
+            for item in search['hits']['hits']:
+                self.found_date_dict[item['_source']['vulnerability']['scanner']['id']] = item['_source']['vulnerability']['found_date']
 
     @property
     def last_updated_query(self):
