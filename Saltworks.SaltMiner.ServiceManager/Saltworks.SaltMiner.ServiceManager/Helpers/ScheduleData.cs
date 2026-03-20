34bfb529f97dba483d2b79ade05538268d214055
tdsalty <51519226+tdsalty@users.noreply.github.com>
2026-03-20 14:19:31 -0500

    moar debugging!

--- Saltworks.SaltMiner.ServiceManager/Saltworks.SaltMiner.ServiceManager/Helpers/ScheduleData.cs
+++ Saltworks.SaltMiner.ServiceManager/Saltworks.SaltMiner.ServiceManager/Helpers/ScheduleData.cs
@@ -249,7 +249,7 @@ public class ScheduleData(ILogger<ScheduleData> logger, DataClientFactory<DataCl
             foreach (var excludedJob in excludedJobs)
             {
                 Logger.LogInformation("[ScheduleData] Job {ExcludedJobName} has been deleted from service jobs. Removing from scheduler", excludedJob.Name);
-                Logger.LogDebug("[ScheduleData] JobKeys: [{Jobkeys}]", queueJobKeys.Select(x => x.Name).ToArray());
+                Logger.LogDebug("[ScheduleData] JobKeys: [{Jobkeys}]", string.Join(',', queueJobKeys.Select(x => x.Name)));
 
                 jobCount--;
                 try
