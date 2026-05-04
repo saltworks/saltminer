596582656fad98cebaf668defe4422342b329c13
cameron <cameron@saltworks.io>
2026-05-04 11:24:05 -0600

    setting stricit port DTO with default in SmDocsAndDTOs

--- Saltworks.SaltMiner.Python/Core/SmDocsAndDTOs.py
+++ Saltworks.SaltMiner.Python/Core/SmDocsAndDTOs.py
@@ -189,6 +189,7 @@ class Asset(BaseModel):
     Instance: Optional[str] = None
     Attributes: Dict[str, Any] = {}
     LastScanDaysPolicy: str = "30"
+    Port: int = 0 
 
 class SaltminerAsset(BaseModel):
     Asset: Asset
