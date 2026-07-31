ee22e55ad10a945cb755cb581ac211e9617ed3fb
Travis <travisldickson@hotmail.com>
2026-07-31 10:21:32 -0500

    bugfix logging

--- Saltworks.SaltMiner.Python/RunAgentService.py
+++ Saltworks.SaltMiner.Python/RunAgentService.py
@@ -1,24 +1,16 @@
 import logging
-import os
 import sys
 
 from Core.Application import Application
 from Core.Agent import Agent, AgentArgs
 from Sources.SyncWorker import SyncWorkerFactory, SyncQueueStage
 
-sourceNames = []
-prog = os.path.splitext(os.path.basename(__file__))[0]
-prm_service = True
-prm_log_tag = None
-if len(sys.argv) > 1:
-    prm_service = bool(sys.argv[1])     # Run as service, defaults to True, will keep going until stopped
-if len(sys.argv) > 2:
-    prm_log_tag = sys.argv[2]     # Custom logging tag, defaults to none
-
-app = Application(loggingCustomTag=prm_log_tag)
 
 def main():
-    app = Application()
+    prm_service = bool(sys.argv[1]) if len(sys.argv) > 1 else True   # run as service until stopped
+    prm_log_tag = sys.argv[2] if len(sys.argv) > 2 else None         # custom logging tag
+
+    app = Application(loggingCustomTag=prm_log_tag)
     agent_args = AgentArgs(
         queue_index_pattern_tag = "sync",
         low_threshold_count = app.Settings.Get("SyncAgent", "LowThresholdCount", 10),
@@ -33,4 +25,6 @@ def main():
     agent.run(stop_when_empty=not prm_service)
     logging.info("Sync Agent stopped")
 
-main()
+
+if __name__ == "__main__":
+    main()
